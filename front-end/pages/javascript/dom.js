5995ba88b59f5790e10794f3bc596a926f811fcb
matfreecss <Mateus14HI@gmail.com>
2025-07-01 22:04:40 -0300

    Adicionado dom.html, dom.js, dom.css
    Foi adicionado dom.html, dom.js, dom.css. Também foi movido todas as páginas .css para página "css", também foi adicionado dom.js na página javascript.

---
+++ front-end/pages/javascript/dom.js
@@ -0,0 +1,19 @@
+// document.writeln("Hello World!")
+
+// var container = document.getElementById('container') // pegando div do html
+// var elemento = document.createElement('button') // criando um botão
+// elemento.textContent = 'Button' // define o conteúdo textual do elemento
+// // elemento.style.backgroundColor = '#ff0000' // é possível estilizar os elementos
+// elemento.id = 'btn' // adiciona um id ao elemento
+// elemento.className = 'btn' //define uma classse ao elemento, 
+// // .classList pode ser usado se for adicionar de uma class
+
+// container.appendChild(elemento) // adicioan um elemento filho
+
+
+// container.innerHTML += `<button class="square">square</button>` // literal com valores
+ 
+
+
+
+
