2231d334b308b39aab848493034d37fd949f4c24
matfreecss <Mateus14HI@gmail.com>
2025-07-07 21:11:55 -0300

    Jogo
    Foi adicionado "jogo.html" na pasta front-end, "jogo.css" na pasta css e "jogo.js" na pasta javascript.

---
+++ front-end/pages/javascript/jogo.js
@@ -0,0 +1,22 @@
+
+var container = document.getElementById('container')
+flag = true
+function CreateSquare(){
+    if(document.querySelector('.square')!==null){
+        document.querySelector('.square').remove()
+    }
+    let a = document.createElement('div')
+    a.className = 'square'
+    a.addEventListener('click', ()=>{
+        CreateSquare()
+    })
+    if(flag){
+        a.style.backgroundColor = "blue"
+    } else{
+        a.style.backgroundColor = "red"
+    }
+    flag = !flag
+    container.appendChild(a)
+}
+
+CreateSquare()
