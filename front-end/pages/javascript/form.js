8a75122a147046479fae4124f502937b61e6b307
matfreecss <Mateus14HI@gmail.com>
2025-07-09 22:01:33 -0300

    Criado "form.js + string.js
    Foi criado "form.js" na página javascript e "string.js" na página script.

---
+++ front-end/pages/javascript/form.js
@@ -0,0 +1,6 @@
+// var container = document.getElementById('container')
+// container.innerHTML =  `<div id="card">
+//             <div class="top"></div>
+//             <div class="square"></div>
+//         </div>
+//     </div>`
---
+++ front-end/scripts/string.js
@@ -0,0 +1,19 @@
+var text = 'O rato roeu a roupa do rei de roma'
+var palavras = text.split('')
+for(let i = 0; i< palavras.length; i++){
+     alert(palavras[i])
+    if("rato" === palavras[i])
+        palavras[i] = 'cachorro'
+
+// }
+// alert(palavras.join(' ').replace('roma', 'grécia'))
+
+// Faça um script que conte quantas vezes a letra 'o' (minúsculo na frase) aparece
+var soma = 0
+for (let i = 0; i < palavras.length[i];i++)
+    if('o' === palavra[i]){
+        soma++
+    }
+}
+alert(soma)
+alert(text)
