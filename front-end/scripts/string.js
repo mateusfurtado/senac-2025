2095845d692246bf1a4e18549f82ad7fd616998f
matfreecss <Mateus14HI@gmail.com>
2025-07-11 22:01:40 -0300

    Criado "semantico.js" + "semantico.html" + "semantico.css"
    Foi criado "semantico.js" , "semantico.js" e "semantico.css"

--- front-end/scripts/string.js
+++ front-end/scripts/string.js
@@ -1,19 +1,22 @@
-var text = 'O rato roeu a roupa do rei de roma'
-var palavras = text.split('')
-for(let i = 0; i< palavras.length; i++){
-     alert(palavras[i])
-    if("rato" === palavras[i])
-        palavras[i] = 'cachorro'
-
-// }
-// alert(palavras.join(' ').replace('roma', 'grécia'))
-
-// Faça um script que conte quantas vezes a letra 'o' (minúsculo na frase) aparece
-var soma = 0
-for (let i = 0; i < palavras.length[i];i++)
-    if('o' === palavra[i]){
-        soma++
+function Stringg() {
+    var text = 'O rato roeu a roupa do rei de roma'
+    var palavras = text.split('')
+    for (let i = 0; i < palavras.length; i++) {
+        //alert(palavras[i])
+        if ("rato" === palavras[i]) {
+            palavras[i] = 'cachorro'
+        }
     }
+    //alert(palavras.join(' ').replace('roma', 'grécia'))
+
+    // Faça um script que conte quantas vezes a 
+    // letra 'o' (minúscula) aparece na frase.
+    var soma = 0
+    for (let i = 0; i < palavras.length; i++) {
+        if ("o" === palavras[i]) {
+            soma++
+        }
+    }
+    alert(soma)
+    alert(text)
 }
-alert(soma)
-alert(text)
