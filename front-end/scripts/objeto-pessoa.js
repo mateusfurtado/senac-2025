27a09d6ad7693e303bf844775ee87c81447608cc
matfreecss <Mateus14HI@gmail.com>
2025-07-17 22:01:28 -0300

    Pequenas correções.

--- front-end/scripts/objeto-pessoa.js
+++ front-end/scripts/objeto-pessoa.js
@@ -5,15 +5,14 @@
 
 class Pessoa {
     constructor(id, nome, sobrenome, idade) {
-    this.id = id;
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    this.idade = idade;
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
     }
-    
-}
 
-detalhes() {
-    return `ID: ${this.id}, Nome: ${this.nome}, Sobrenome: ${this.sobrenome}, Idade: ${this.idade}`
+    detalhes() {
+        return `ID: ${this.id}, Nome: ${this.nome}, Sobrenome: ${this.sobrenome}, Idade: ${this.idade}`
 
+    }
 }
